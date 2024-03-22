b2d01510cf7fe7d179a173507eb611ea22fd28ea
DanielKOSTA <dlsclwnssla@gmail.com>
2024-03-22 16:38:40 +0900

    3/22 study

---
+++ string/strcmp.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+
+/*
+int strcmp(const char* s1, const char* s2){
+	return 1;
+}
+*/
+
+int main(){
+	char* str1 = "hello";
+	char* str2 = "hello!";
+
+	printf("%d\n", strcmp(str1, str2);
+
+	return 1;
+}
