ed32da6e5d0a811b44a0718a209f727a8f7703f4
DanielKOSTA <dlsclwnssla@gmail.com>
2024-03-29 17:42:50 +0900

    feat: write strcmp.c

--- string/strcmp.c
+++ string/strcmp.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 #include <string.h>
 
-/*
 int strcmp(const char* s1, const char* s2){
+	for(int i = 0; s1
+
 	return 1;
 }
-*/
 
 int main(){
-	char* str1 = "hello";
-	char* str2 = "hello!";
+	char* str1 = "helloa!";
+	char* str2 = "hellobc";
 
-	printf("%d\n", strcmp(str1, str2);
+	printf("%d\n", strcmp(str1, str2));
 
 	return 1;
 }
